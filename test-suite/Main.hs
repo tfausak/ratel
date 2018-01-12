24f348b49f3d8a3b5fd267059bd235fe3c1fe931
Taylor Fausak <taylor@fausak.me>
2018-01-12 07:49:39 -0600

    Use hspec instead of tasty
    https://github.com/fpco/stackage/issues/3178

--- test-suite/Main.hs
+++ test-suite/Main.hs
@@ -1,8 +1,31 @@
-import qualified RatelSpec
-import qualified Test.Tasty
-import Test.Tasty.Hspec
+import qualified Control.Exception as Exception
+import qualified Ratel
+import qualified System.FilePath as FilePath
+import qualified Test.Hspec as Hspec
 
 main :: IO ()
-main = do
-    test <- testSpec "ratel" (parallel RatelSpec.spec)
-    Test.Tasty.defaultMain test
+main = Hspec.hspec . Hspec.describe "Ratel" $
+  Hspec.describe "toError" $
+    Hspec.it "converts an Exception into an Error" $
+      Exception.catch
+        (do
+          _ <- Exception.throwIO (userError "something went wrong")
+          True `Hspec.shouldBe` False)
+        (\ exception -> do
+          let
+            file = FilePath.combine "test-suite" "Main.hs"
+            actual = Ratel.toError (exception :: Exception.SomeException)
+            expected = Ratel.Error
+              { Ratel.errorBacktrace = Just
+                [ Ratel.Trace
+                  { Ratel.traceFile = Just file
+                  , Ratel.traceMethod = Just "Main.toError"
+                  , Ratel.traceNumber = Just "17:22"
+                  }
+                ]
+              , Ratel.errorClass = Just "SomeException"
+              , Ratel.errorMessage = Just "user error (something went wrong)"
+              , Ratel.errorSource = Nothing
+              , Ratel.errorTags = Nothing
+              }
+          actual `Hspec.shouldBe` expected)
