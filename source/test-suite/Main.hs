9d35077b4f599bcfe42df73d488b3c9d1f25bb3b
Taylor Fausak <taylor@fausak.me>
2022-01-09 23:19:52 +0000

    Update everything for 2022

--- source/test-suite/Main.hs
+++ source/test-suite/Main.hs
@@ -4,28 +4,32 @@ import qualified System.FilePath as FilePath
 import qualified Test.Hspec as Hspec
 
 main :: IO ()
-main = Hspec.hspec . Hspec.describe "Ratel" $
-  Hspec.describe "toError" $
-    Hspec.it "converts an Exception into an Error" $
-      Exception.catch
+main =
+  Hspec.hspec
+    . Hspec.describe "Ratel"
+    . Hspec.describe "toError"
+    . Hspec.it "converts an Exception into an Error"
+    $ Exception.catch
         (do
           _ <- Exception.throwIO (userError "something went wrong")
-          True `Hspec.shouldBe` False)
-        (\ exception -> do
+          True `Hspec.shouldBe` False
+        )
+        (\exception -> do
           let
-            file = FilePath.joinPath ["src", "test", "Main.hs"]
+            file = FilePath.joinPath ["source", "test-suite", "Main.hs"]
             actual = Ratel.toError (exception :: Exception.SomeException)
             expected = Ratel.Error
               { Ratel.errorBacktrace = Just
                 [ Ratel.Trace
-                  { Ratel.traceFile = Just file
-                  , Ratel.traceMethod = Just "Main.toError"
-                  , Ratel.traceNumber = Just "17:22"
-                  }
+                    { Ratel.traceFile = Just file
+                    , Ratel.traceMethod = Just "Main.toError"
+                    , Ratel.traceNumber = Just "20:22"
+                    }
                 ]
               , Ratel.errorClass = Just "SomeException"
               , Ratel.errorMessage = Just "user error (something went wrong)"
               , Ratel.errorSource = Nothing
               , Ratel.errorTags = Nothing
               }
-          actual `Hspec.shouldBe` expected)
+          actual `Hspec.shouldBe` expected
+        )
