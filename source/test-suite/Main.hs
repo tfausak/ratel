cd997fc48cdf3cffc5fdbe5e142872c299eeb941
Taylor Fausak <taylor@fausak.me>
2024-05-19 08:56:30 -0500

    Fix test

--- source/test-suite/Main.hs
+++ source/test-suite/Main.hs
@@ -1,4 +1,5 @@
 import qualified Control.Exception as Exception
+import qualified Data.Maybe as Maybe
 import qualified Ratel
 import qualified System.FilePath as FilePath
 import qualified Test.Hspec as Hspec
@@ -24,13 +25,17 @@ main =
                         [ Ratel.Trace
                             { Ratel.traceFile = Just file,
                               Ratel.traceMethod = Just "Main.toError",
-                              Ratel.traceNumber = Just "19:24"
+                              Ratel.traceNumber = Just "20:24"
                             }
                         ],
                     Ratel.errorClass = Just "SomeException",
-                    Ratel.errorMessage = Just "user error (something went wrong)",
+                    Ratel.errorMessage = Just "varies by GHC version",
                     Ratel.errorSource = Nothing,
                     Ratel.errorTags = Nothing
                   }
-          actual `Hspec.shouldBe` expected
+          Ratel.errorBacktrace actual `Hspec.shouldBe` Ratel.errorBacktrace expected
+          Ratel.errorClass actual `Hspec.shouldBe` Ratel.errorClass expected
+          Ratel.errorMessage actual `Hspec.shouldSatisfy` Maybe.isJust
+          Ratel.errorSource actual `Hspec.shouldBe` Ratel.errorSource expected
+          Ratel.errorTags actual `Hspec.shouldBe` Ratel.errorTags expected
       )
